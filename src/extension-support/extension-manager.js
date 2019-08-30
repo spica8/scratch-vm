39469dde7678eda09c9299357c2ed91060047157
spica8 <spica8@h5.dion.ne.jp>
2019-08-30 09:48:45 +0900

    block name changed
    block name changed

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -24,7 +24,7 @@ const builtinExtensions = {
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
     gdxfor: () => require('../extensions/scratch3_gdx_for'),
-    sbmltl: () => require('../extensions/scratch3_sbmltl')
+    bmltl: () => require('../extensions/scratch3_bmltl')
 };
 
 /**
