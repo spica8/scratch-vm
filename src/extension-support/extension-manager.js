5698102c85836866fc3205a56499668bf21ec3c2
spica8 <spica8@h5.dion.ne.jp>
2019-09-12 21:54:13 +0900

    modified

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -24,7 +24,9 @@ const builtinExtensions = {
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
     gdxfor: () => require('../extensions/scratch3_gdx_for'),
-    bmltl: () => require('../extensions/scratch3_bmltl')
+    bmltl: () => require('../extensions/scratch3_bmltl'),
+    bmlic: () => require('../extensions/scratch3_bmlic'),
+    bmlknn: () => require('../extensions/scratch3_bmlknn')
 };
 
 /**
